9af8b719e49333a2b096b1c6928b5a5341b7d9d5
Justin Poehl <justinpoehl@hotmail.com>
2018-11-17 16:28:37 +0100

    Incomplete debug Linux

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 import os, signal
-def test(var):
+def test():
+  var = input("Zahl ")
   if var == 1:
-    return "1 gedrückt"
+    return "1 gedrueckt"
   else:
+    print("was anderes gedrueckt")
+    sleep(3)
     os.kill(os.getpid(), signal.SIGTERM) 
 def name():
-  name = input("Your name?: ")
+  name = input("Username: ")
   if name == "Beans" or name == "Justin":
     return "Welcome, " + name
   else:
     return "You are not welcome!!!"
 def close():
-  password = input("Your Password: ")
+  password = input("Your password: ")
   while password != "Python":
     password = input("Your Password: ")
   os.kill(os.getpid(), signal.SIGTERM) 
-print(test(int(input())))
+print(test())
 print(name())
 print("Last step...")
 print("Feierabend")
