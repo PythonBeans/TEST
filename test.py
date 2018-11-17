7604c8c29ac578acd378b1cbaf649e5e6c5f62cc
Justin Pöhl <runninjp@aol.de>
2018-11-17 01:03:50 +0100

    Add password control and exit.

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
+import os, signal
 def test(var):
   if var == 1:
     return "1 gedrückt"
   else:
-    return exit()
+    os.kill(os.getpid(), signal.SIGTERM) 
 def name():
   name = input("Your name?: ")
   if name == "Beans" or name == "Justin":
     return "Welcome, " + name
   else:
     return "You are not welcome!!!"
+def close():
+  password = input("Your Password: ")
+  while password != "Python":
+    password = input("Your Password: ")
+  os.kill(os.getpid(), signal.SIGTERM) 
 print(test(int(input())))
 print(name())
+print(close())
 
